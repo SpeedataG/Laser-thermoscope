cd37ace8ccb600d8dda6fef30902e7f742ea6030
EchoXBR <372183365@qq.com>
2019-12-09 17:32:49 +0800

    添加注释

---
+++ README.md
@@ -0,0 +1,5 @@
+# 说明
+* 主要参考 speedatacom.tn9hongwaitemp.RedActivity 这个类即可
+* 读回来的串口数据  解析参看文档中的信息格式说明  如下图
+
+![Image text](红外数据格式.png)
--- app/src/main/java/speedatacom/tn9hongwaitemp/RedActivity.java
+++ app/src/main/java/speedatacom/tn9hongwaitemp/RedActivity.java
@@ -17,31 +17,120 @@ import util.DataConversionUtils;
 import util.DeviceControl;
 
 public class RedActivity extends Activity implements View.OnClickListener {
-    SerialPort serialPort;
-    DeviceControl control;
-    TextView tvmubian, tvhuanjing, tvMAX, tvMIN, btnMAX, btnMIN, btnStart;
-    int fd;
-    private Thread thread;
     private final String TAG = "RedActivity";
-    float min = 0;
+
+    /**
+     * 串口对象
+     */
+    private SerialPort serialPort;
+    /**
+     * 串口文件句柄  根据句柄进行读写操作
+     */
+    private int fd;
+    /**
+     * 给模块上电对象
+     */
+    private DeviceControl control;
+
+    private TextView tvmubian, tvhuanjing, tvMAX, tvMIN, btnMAX, btnMIN, btnStart;
+
+    private float min = 0;
+
+    /**
+     * 串口数据解析
+     */
+    android.os.Handler handler = new android.os.Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.d(TAG, "handle message");
+            switch (msg.what) {
+                case 0:
+                    byte[] temp = (byte[]) msg.obj;
+                    //目标温度
+                    byte[] mubiaotemp = new byte[2];
+                    // 环境温度
+                    byte[] huanjingtemp = new byte[2];
+                    for (int i = 0; i < temp.length; i += 5) {
+                        // 头为0x4c 对应十进制为76  则此组数据为目标温度
+                        if (temp[i] == 76) {
+
+                            byte sum = (byte) (temp[i] + temp[i + 1] + temp[i + 2]);
+                            byte ww = temp[i + 3];
+                            if (sum == ww) {
+                                //copy数组从第三位开始 cop3个字节 复制放到新数组第零位开始放 copy数组
+                                System.arraycopy(temp, i + 1, mubiaotemp, 0, 2);
+                                float mubiao = (float) DataConversionUtils.byteArrayToInt(mubiaotemp);
+                                float ss = mubiao / 16;
+                                float m = (float) (ss - 273.15);
+                                String mresult = saveDecimals(m);
+                                if (m > 100) {
+                                    tvmubian.setTextColor(Color.RED);
+                                } else {
+                                    tvmubian.setTextColor(Color.WHITE);
+                                }
+                                tvmubian.setText(mresult + "℃");
+                                if (m > min) {
+                                    tvMAX.setText(mresult);
+                                } else {
+                                    tvMIN.setText(mresult);
+                                }
+                                min = m;
+                            } else {
+                                Log.e(TAG, "check error mubiao  " + DataConversionUtils.byteArrayToStringLog(temp, temp.length));
+                                return;
+                            }
+                        }
+                        // 头为0x66 对应十进制为102  则此组数据为环境温度
+                        else if (temp[i] == 102) {
+                            byte sum = (byte) (temp[i] + temp[i + 1] + temp[i + 2]);
+                            if (sum == temp[i + 3]) {
+                                //copy数组从第三位开始 cop3个字节 复制放到新数组第零位开始放 copy数组
+                                System.arraycopy(temp, i + 1, huanjingtemp, 0, 2);
+                                float huanjing = (float) DataConversionUtils.byteArrayToInt(huanjingtemp);
+                                float ss = huanjing / 16;
+                                float h = (float) (ss - 273.15);
+                                String hresult = saveDecimals(h);
+                                tvhuanjing.setText(hresult + "℃");
+                            } else {
+                                Log.e(TAG, "check error huanjing");
+                                return;
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    };
+    private Thread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_read);
 
+
+        initModule();
+        initUI();
+    }
+
+    /**
+     * 初始化红外测温模块
+     */
+    private void initModule() {
         try {
+            //打开串口
             serialPort = new SerialPort();
             serialPort.OpenSerial("/dev/ttyMT2", 9600);
             fd = serialPort.getFd();
+            //实例化模块上电对象
             control = new DeviceControl("/sys/class/misc/mtgpio/pin");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        init();
     }
 
-    private void init() {
+    private void initUI() {
         tvmubian = (TextView) findViewById(R.id.tv_mubiao);
         tvhuanjing = (TextView) findViewById(R.id.tv_huanjing);
         tvMAX = (TextView) findViewById(R.id.tv_max);
@@ -49,7 +138,7 @@ public class RedActivity extends Activity implements View.OnClickListener {
         btnMAX = (TextView) findViewById(R.id.btn_max);
         btnMIN = (TextView) findViewById(R.id.btn_min);
         btnStart = (TextView) findViewById(R.id.btn_start);
-//        btnStart.setOnClickListener(this);
+        //        btnStart.setOnClickListener(this);
         btnStart.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent motionEvent) {
@@ -77,63 +166,6 @@ public class RedActivity extends Activity implements View.OnClickListener {
 
     }
 
-    android.os.Handler handler = new android.os.Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            Log.d(TAG, "handle message");
-            switch (msg.what) {
-                case 0:
-                    byte[] temp = (byte[]) msg.obj;
-                    byte[] mubiaotemp = new byte[2];
-                    byte[] huanjingtemp = new byte[2];
-                    for (int i = 0; i < temp.length; i += 5) {
-                        if (temp[i] == 76) {//目标温度
-                            byte sum = (byte) (temp[i] + temp[i + 1] + temp[i + 2]);
-                            byte ww = temp[i + 3];
-                            if (sum == ww) {
-
-                                System.arraycopy(temp, i + 1, mubiaotemp, 0, 2);//copy数组从第三位开始 cop3个字节 复制放到新数组第零位开始放 copy数组
-                                float mubiao = (float) DataConversionUtils.byteArrayToInt(mubiaotemp);
-                                float ss = mubiao / 16;
-                                float m = (float) (ss - 273.15);
-                                String mresult = saveDecimals(m);
-                                if (m > 100) {
-                                    tvmubian.setTextColor(Color.RED);
-                                } else {
-                                    tvmubian.setTextColor(Color.WHITE);
-                                }
-                                tvmubian.setText(mresult + "℃");
-                                if (m > min) {
-                                    tvMAX.setText(mresult);
-                                } else {
-                                    tvMIN.setText(mresult);
-                                }
-                                min = m;
-                            } else {
-                                Log.e(TAG, "check error mubiao  " + DataConversionUtils.byteArrayToStringLog(temp, temp.length));
-                                return;
-                            }
-                        } else if (temp[i] == 102) {//环境温度
-                            byte sum = (byte) (temp[i] + temp[i + 1] + temp[i + 2]);
-                            if (sum == temp[i + 3]) {
-                                System.arraycopy(temp, i + 1, huanjingtemp, 0, 2);//copy数组从第三位开始 cop3个字节 复制放到新数组第零位开始放 copy数组
-                                float huanjing = (float) DataConversionUtils.byteArrayToInt(huanjingtemp);
-                                float ss = huanjing / 16;
-                                float h = (float) (ss - 273.15);
-                                String hresult = saveDecimals(h);
-                                tvhuanjing.setText(hresult + "℃");
-                            } else {
-                                Log.e(TAG, "check error huanjing");
-                                return;
-                            }
-                        }
-                    }
-                    break;
-            }
-        }
-    };
-
     public String saveDecimals(float f) {
         DecimalFormat df = new DecimalFormat("#,##0.00");
         String dff = df.format(f);
@@ -145,7 +177,27 @@ public class RedActivity extends Activity implements View.OnClickListener {
 
     }
 
+    @Override
+    protected void onStop() {
+        //        停止读串口
+        if (thread != null) {
+            thread.interrupt();
+        }
+        // 下电
+        control.PowerOffDevice("94");
+        control.PowerOffDevice("93");
+        super.onStop();
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * 线程，阻塞读串口，有数据通过handler发送到UI线程
+     */
     private class ReadThread extends Thread {
         @Override
         public void run() {
@@ -163,21 +215,5 @@ public class RedActivity extends Activity implements View.OnClickListener {
                     e.printStackTrace();
                 }
         }
-    }
-
-
-    @Override
-    protected void onStop() {
-        if (thread != null) {
-            thread.interrupt();
-        }
-        control.PowerOffDevice("94");
-        super.onStop();
-        control.PowerOffDevice("93");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 }
